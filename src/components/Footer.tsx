62c72269ecb7c86003ad3dcf307a15daf7cb07a7
Irfan Maulana <mazipanneh@gmail.com>
2023-09-18 09:35:52 +0700

    feat: add more links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,77 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className='text-center p-4 border-t'>
-      <div className="flex gap-4 justify-center items-center mb-4">
-        <Link href="/ketentuan-layanan" className="underline">
-         Ketentuan Layanan
-        </Link>
-        <span>•</span>
-        <Link href="/kebijakan-privasi"  className="underline">
-         Kebijakan Privasi
-        </Link>
+    <footer className='p-4 border-t'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 container mb-8">
+        <div>
+          <h4 className="text-lg font-bold mb-2">Lebih banyak</h4>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <Link href="/tentang" className="underline">
+                Tentang
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="text-lg font-bold mb-2">Kebijakan</h4>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <Link href="/ketentuan-layanan" className="underline">
+                Ketentuan Layanan
+              </Link>
+            </li>
+            <li>
+              <Link href="/kebijakan-privasi" className="underline">
+                Kebijakan Privasi
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="text-lg font-bold mb-2">Sumber daya</h4>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <a href="https://github.com/mazipan/tanyaaja/issues/new" className="underline" target='_blank' rel='noopener noreferrer'>
+                Laporkan Isu
+              </a>
+            </li>
+            <li>
+              <a href="https://mazipan.space/support" className="underline" target='_blank' rel='noopener noreferrer'>
+                Dukung
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="text-lg font-bold mb-2">Karya Lain</h4>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <a href="https://ksana.in" className="underline" target='_blank' rel='noopener noreferrer'>
+                Ksana.in
+              </a>
+            </li>
+            <li>
+              <a href="https://www.baca-quran.id/" className="underline" target='_blank' rel='noopener noreferrer'>
+                Baca-Quran.id
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <p><small>© Sejak 2023, TanyaAja</small></p>
-      <p><small>Oleh <a href="https://mazipan.space/" target='_blank' rel='noopener noreferrer' className='underline'>Irfan Maulana</a></small></p>
+      <div className="container">
+        <div className="text-center">
+          <p>
+            <small>© Sejak 2023, TanyaAja</small>
+          </p>
+          <p>
+            <small>
+              <span>With ☕️, by </span>
+              <a href="https://mazipan.space/" target='_blank' rel='noopener noreferrer' className='underline'> Irfan Maulana</a>
+            </small>
+          </p>
+        </div>
+      </div>
     </footer>
   )
 }
