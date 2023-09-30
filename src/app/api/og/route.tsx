b7b6ea1cf110e93619630077939ec96c826e057f
Irfan Maulana <mazipanneh@gmail.com>
2023-09-30 15:24:56 +0700

    Update route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -77,9 +77,9 @@ function QuestionOg({ question }: { question: string }) {
     <div tw="flex p-10 flex-col w-full h-full items-center justify-center bg-white">
       <div tw="flex flex-col justify-center items-center font-extrabold text-3xl tracking-tight w-full">
         <p>
-          {question.length > 500
-            ? `${question.substring(0, 500)}...`
-            : question}
+          {question?.length > 500
+            ? `${question?.substring(0, 500)}...`
+            : `${question}`}
         </p>
       </div>
       <div></div>
