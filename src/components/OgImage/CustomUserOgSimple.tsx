3fb676a68804f23745b932c6265d13f84cc13b9c
Irfan Maulana <mazipanneh@gmail.com>
2023-10-09 09:06:54 +0700

    chore: use justify-center

--- src/components/OgImage/CustomUserOgSimple.tsx
+++ src/components/OgImage/CustomUserOgSimple.tsx
@@ -14,18 +14,18 @@ export function CustomUserOgSimple({
 }) {
   return (
     <div
-      tw={`flex p-8 flex-col w-full h-full items-center justify-between rounded-3xl`}
+      tw={`flex p-8 flex-col w-full h-full items-center justify-center rounded-3xl`}
       style={{
         backgroundImage: GRADIENTS.find((g) => g.id === theme)?.cssNative,
       }}
     >
-      <div tw="flex font-bold text-2xl">
+      <div tw="flex font-bold text-2xl mb-4">
         {BASEURL.replace('https://www.', '')}/p/{slug}
       </div>
       <div tw="flex flex-col justify-center items-center text-center font-extrabold text-6xl tracking-tight w-full">
         <p>{text}</p>
       </div>
-      <div tw="flex mb-2 items-center justify-center mt-10 w-full">
+      <div tw="flex mb-2 items-center justify-center mt-4 w-full">
         <LogoSvg />
       </div>
     </div>
