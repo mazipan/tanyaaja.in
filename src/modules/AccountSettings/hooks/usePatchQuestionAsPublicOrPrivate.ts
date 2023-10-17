518b34798ae13d71c06294ca30b93afd9fc3bcd7
Patrick Hyatt <phyatt@churnzero.net>
2023-10-17 08:49:13 -0400

    - Fix parse method parameter order

--- src/modules/AccountSettings/hooks/usePatchQuestionAsPublicOrPrivate.ts
+++ src/modules/AccountSettings/hooks/usePatchQuestionAsPublicOrPrivate.ts
@@ -1,16 +1,15 @@
 import { useMutation } from '@tanstack/react-query'
+import { boolean, object, parse, string } from 'valibot'
 
 import { useAuth } from '@/components/FirebaseAuth'
 import { patchQuestionAsPublicOrPrivate } from '@/lib/api'
 import { getFirebaseAuth } from '@/lib/firebase'
 import { Question } from '@/lib/types'
 
-import { boolean, object, parse, string } from 'valibot';
-
 const payloadSchema = object({
   uuid: string(),
   public: boolean(),
-});
+})
 
 const auth = getFirebaseAuth()
 
@@ -20,7 +19,7 @@ export const usePatchQuestionAsPublicOrPrivate = () => {
   const mutation = useMutation({
     mutationFn: (variable?: Question | null) => {
       // Validate
-      const validatedVariable = parse(variable, payloadSchema)
+      const validatedVariable = parse(payloadSchema, variable)
 
       if (!user) throw new Error('User is not logged in')
 
