968fa4d5d2a07b64cbad292f5ccd1977140e87c5
haruelrovix <surat@havit.web.id>
2023-10-23 23:25:48 +0700

    FIX: Firebase app named [NAME] already exists

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,14 @@
-import { initializeApp } from 'firebase-admin/app'
+import { App, getApp, getApps, initializeApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
 import { firebaseConfig } from '@/lib/firebase'
 
-const app = initializeApp(
-  firebaseConfig,
-  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-)
+const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? ''
 
-export const verifyIdToken = (idToken: string) =>
-  getAuth(app).verifyIdToken(idToken)
+const app: App = getApps().length
+  ? getApp(PROJECT_ID)
+  : initializeApp(firebaseConfig, PROJECT_ID)
+
+const Auth = getAuth(app)
+
+export const verifyIdToken = (idToken: string) => Auth.verifyIdToken(idToken)
