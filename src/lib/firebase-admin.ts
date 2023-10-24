cebefe4f4c438787b15509d3edcbc97a349da50f
haruelrovix <surat@havit.web.id>
2023-10-24 10:45:11 +0700

    FEAT: Add method to Revoke refresh token

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,4 +1,10 @@
-import { type App, getApp, getApps, initializeApp } from 'firebase-admin/app'
+import {
+  type App,
+  applicationDefault,
+  getApp,
+  getApps,
+  initializeApp,
+} from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
 import { firebaseConfig } from '@/lib/firebase'
@@ -7,8 +13,17 @@ const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? ''
 
 const app: App = getApps().length
   ? getApp(PROJECT_ID)
-  : initializeApp(firebaseConfig, PROJECT_ID)
+  : initializeApp(
+      {
+        ...firebaseConfig,
+        credential: applicationDefault(),
+      },
+      PROJECT_ID,
+    )
 
 const Auth = getAuth(app)
 
 export const verifyIdToken = (idToken: string) => Auth.verifyIdToken(idToken)
+
+export const revokeRefreshTokens = (uid: string) =>
+  Auth.revokeRefreshTokens(uid)
