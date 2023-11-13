7551d0ed9e83488af994bd056e59eeb370fcb4fd
BillGun <privatebillgun@gmail.com>
2023-11-14 10:29:44 +1100

    FEAT: Added Twitter Screenshot section in Homepage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -210,3 +210,11 @@ export function calculatePageItemCount<T>(arr: ICalculatePageItemCount<T>[]) {
 
   return totalQuestion
 }
+
+export function shuffleArray<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
