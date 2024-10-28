a5d018cbe45c696fd107196a0d4b98f17ce125bb
Havit Rovik <surat@havit.web.id>
2024-10-28 10:19:33 +0700

    FEAT: Delete user (#134)
    Part of https://github.com/mazipan/tanyaaja/issues/3
    
    ## Description
    
    - Add middleware to handle Authorization
    - Add methods to Archive a Page and Delete Questions by `uid`
    - Add endpoint to DELETE user
    
    📝 As stated [here](https://nextjs.org/docs/pages/api-reference/edge#unsupported-apis),
    
    > Native Node.js APIs are not supported
    
    that's why there is a new package added `next-firebase-auth-edge` as a workaround.
    
    Co-authored-by: Irfan Maulana <7221389+mazipan@users.noreply.github.com>

---
+++ src/lib/firebase-auth-edge.ts
@@ -0,0 +1,26 @@
+import { getFirebaseAuth } from 'next-firebase-auth-edge';
+import type { DecodedIdToken } from 'next-firebase-auth-edge/lib/auth/token-verifier';
+
+const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? '';
+const API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '';
+
+// Read: https://dev.to/vvo/how-to-add-firebase-service-account-json-files-to-vercel-ph5
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string,
+)
+
+const Auth = getFirebaseAuth({
+  apiKey: API_KEY,
+  serviceAccount: {
+    projectId: PROJECT_ID,
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+  },
+});
+
+export const verifyIdToken = (
+  idToken: string,
+  checkRevoked?: boolean
+): Promise<DecodedIdToken> => Auth.verifyIdToken(idToken, { checkRevoked });
+
+export const deleteUser = (uid: string) => Auth.deleteUser(uid);
